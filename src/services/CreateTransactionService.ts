a935c7e8c7cc9fcb1dd83ee413ca79511695d9df
jordanjpmg <jordanjpmg@gmail.com>
2020-04-23 23:27:16 -0300

    Migrations criadas

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,23 @@
 // import AppError from '../errors/AppError';
 
+import { getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Transaction);
   }
 }
 
